b20997f5ea08a1c111395005a9a4af15ccf459b3
Abhishek Bondre <abhibondre27@gmail.com>
2023-05-22 15:15:58 +0530

    Creating endpoints for client side

---
+++ doc-client/src/api/endpoints.js
@@ -0,0 +1,12 @@
+export default {
+  serverBaseURL: "http://localhost:6666/api",
+  api: {
+    users: {
+      create: "/users",
+      update: "/users/",
+      delete: "/users/",
+      getOne: "/users/",
+      getAll: "/users",
+    },
+  },
+};
