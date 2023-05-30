82959abd9e573e2a72272a8559be90569c500498
Abhishek Bondre <abhibondre27@gmail.com>
2023-05-30 23:02:04 +0530

    modify...

--- doc-client/src/services/AuthService.js
+++ doc-client/src/services/AuthService.js
@@ -1,9 +1,7 @@
 import { API, endpoints } from "../api";
 
-const AuthService = () => {
-  const userLogin = (user) => {
-    return API.post(endpoints?.api?.auth?.userLogin, user);
-  };
+const userLogin = (user) => {
+  return API.post(endpoints?.api?.auth?.userLogin, user);
 };
 
-export default AuthService;
+export default userLogin;
