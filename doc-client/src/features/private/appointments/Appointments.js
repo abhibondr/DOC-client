6118a89aeb49472bdc72a76f1eb63fcfcec8a3b5
Yogesh Baravkar <barawkaryog@gmail.com>
2023-05-31 20:22:53 +0530

    creating protected pages in private folder

---
+++ doc-client/src/features/private/appointments/Appointments.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Appointments = () => {
+  return (
+    <>
+      <h3>Appointments</h3>
+    </>
+  );
+};
+
+export default Appointments;
---
+++ doc-client/src/features/private/doctors/Doctors.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Doctors = () => {
+  return (
+    <>
+      <h3>Doctors </h3>
+    </>
+  );
+};
+
+export default Doctors;
