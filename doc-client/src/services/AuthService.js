24dcdc3132d2e5b18e28d6294d5989db60014deb
Abhishek Bondre <abhibondre27@gmail.com>
2023-05-31 23:04:28 +0530

    validate the token...

--- doc-client/src/services/AuthService.js
+++ doc-client/src/services/AuthService.js
@@ -4,5 +4,11 @@ const AuthService = {
   userLogin(user) {
     return API.post(endpoints?.api?.auth?.userLogin, user);
   },
+
+  validateToken() {
+    const token = sessionStorage.getItem("token");
+    if (token) return API.post(endpoints?.api?.auth?.validateToken, { token });
+    else return Promise.reject("Token not available");
+  },
 };
 export default AuthService;
