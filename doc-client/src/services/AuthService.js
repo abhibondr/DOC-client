0c547c8113f6b44c7b750e308fabbdaad899df91
Abhishek Bondre <abhibondre27@gmail.com>
2023-06-01 15:06:06 +0530

    modify authservice...

--- doc-client/src/services/AuthService.js
+++ doc-client/src/services/AuthService.js
@@ -7,6 +7,7 @@ const AuthService = {
 
   validateToken() {
     const token = sessionStorage.getItem("token");
+
     if (token) return API.post(endpoints?.api?.auth?.validateToken, { token });
     else return Promise.reject("Token not available");
   },
