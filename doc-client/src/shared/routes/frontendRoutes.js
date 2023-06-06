e0b35d71d6883802b9a8ca1faf5672056826a20e
Abhishek Bondre <abhibondre27@gmail.com>
2023-06-06 15:43:26 +0530

    modify

--- doc-client/src/shared/routes/frontendRoutes.js
+++ doc-client/src/shared/routes/frontendRoutes.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-anonymous-default-export */
 import React, { lazy } from "react";
 
 import HomeIcon from "@mui/icons-material/Home";
@@ -8,7 +7,6 @@ import ContactIcon from "@mui/icons-material/ContactPage";
 import AboutIcon from "@mui/icons-material/MedicalInformation";
 import ServicesIcon from "@mui/icons-material/MedicalServices";
 import TestimonialIcon from "@mui/icons-material/Comment";
-import Footer from "../../features/footer/Footer";
 
 const HomePage = lazy(() => import("../../features/home/HomePage"));
 const AboutPage = lazy(() => import("../../features/about/AboutPage"));
