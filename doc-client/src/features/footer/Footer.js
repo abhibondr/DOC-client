0be71627e476ddc5af8529ffd2b7a6cdf0cf1ff5
Pornima Chndanphule <pornimachandanphule345@gmail.com>
2023-06-11 19:59:09 +0530

    services and footer created

--- doc-client/src/features/footer/Footer.js
+++ doc-client/src/features/footer/Footer.js
@@ -1,5 +1,6 @@
 import { Container, Grid, TableContainer, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 import Facebook from "@mui/icons-material/Facebook";
 import Instagram from "@mui/icons-material/Instagram";
@@ -8,69 +9,88 @@ import Youtube from "@mui/icons-material/YouTube";
 const Footer = () => {
   return (
     <>
-      <div style={{ backgroundColor: "gray" }} className="parent">
+      <div className="parent">
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography variant="h3" sx={{ margin: "15px" }}>
+            <Typography variant="h5" marginLeft="20px">
               Doctor <br />
               Appointment
             </Typography>
-            <Typography variant="body2" sx={{ margin: "15px" }}>
-              Doctor Appointment is a registered start up company empanelled
-              with 20 plus doctors and 500 patients lives touched.
+            <Typography variant="body2" marginLeft="20px">
+              Doctor Appointment is a registered start up company
+              <br /> empanelled with 20 plus doctors and 500 patients lives
+              touched.
             </Typography>
-            <Facebook fontSize="large" />
-            <Instagram fontSize="large" />
-            <Twitter fontSize="large" />
-            <Youtube fontSize="large" />
+            <Typography marginLeft="20px">
+              <Facebook fontSize="small" />
+              <Instagram fontSize="small" />
+              <Twitter fontSize="small" />
+              <Youtube fontSize="small" />
+            </Typography>
           </Grid>
           <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography variant="h4" sx={{ margin: "15px" }}>
+            <Typography variant="h6" marginLeft="20px">
               For Information
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
-              - About Us
+            <Typography variant="body2" marginLeft="20px">
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/about"
+              >
+                - About Us
+              </Link>
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
-              -Booking Guides
+
+            <Typography variant="body2" marginLeft="20px">
+              - Booking Guides
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
+
+            <Typography variant="body2" marginLeft="20px">
               - Pharmacy
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
-              -Careers
+
+            <Typography variant="body2" marginLeft="20px">
+              - Careers
             </Typography>
           </Grid>
           <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography variant="h4" sx={{ margin: "15px" }}>
+            <Typography variant="h6" marginLeft="20px">
               Helpful Links
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
-              -Booking Appointment
+            <Typography variant="body2" marginLeft="20px">
+              - Booking Appointment
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
+
+            <Typography variant="body2" marginLeft="20px">
               - SMS Booking
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
-              - Services
+
+            <Typography variant="body2" marginLeft="20px">
+              <Link
+                to="/services"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                - Services
+              </Link>
             </Typography>
           </Grid>
           <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography variant="h4" sx={{ margin: "15px" }}>
+            <Typography variant="h6" marginLeft="20px">
               Contact Us
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
-              - Ankur Hospital PVT LTD,
-              <br />
-              Nanded,Maharashtra.
+            <Typography variant="body2" marginLeft="20px">
+              - Ankur Hospital PVT LTD.
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
+
+            <Typography variant="body2" marginLeft="20px">
               - +91 8208224984
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
+
+            <Typography variant="body2" marginLeft="20px">
               - Customer Support: poonam123@gmail.com
             </Typography>
-            <Typography variant="h6" sx={{ margin: "15px" }}>
+
+            <Typography variant="body2" marginLeft="20px">
               - Official Queries: abhi123@gmail.com
             </Typography>
           </Grid>
