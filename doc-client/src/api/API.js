9bd26e0a6d7c5f31911e92558176ceb767ef3a48
Abhishek Bondre <abhibondre27@gmail.com>
2023-06-12 11:31:59 +0530

    adding query API

--- doc-client/src/api/API.js
+++ doc-client/src/api/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import endpoints from "./endpoints";
 
 const API = axios.create({
-  baseURL: endpoints?.serverBaseURL,
+  baseURL: endpoints?.serverBaseURL + "/api",
 });
 
 export default API;
